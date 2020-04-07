fd8ba10150b6dfbd3cbd04a0d1bd7c082224efdd
Sergio Boatella <sergioboatella@gmail.com>
2020-04-07 19:57:47 +0200

    Delete temporal files

--- src/embedded_controller.hpp
+++ src/embedded_controller.hpp
@@ -13,21 +13,30 @@
     
 #include <cinttypes>
 #include <exception>
- const uint32_t MAX_MEMORY_RANGE_ADDRESS = UINT32_MAX;        // Maximum memory range definition
+
+ 
+const uint32_t MAX_MEMORY_RANGE_ADDRESS = UINT32_MAX;        // Maximum memory range definition
 
 /** Data structure for storing base address
  * 
  *  @member baseAddress Base memory address
  */ 
-typedef struct DataContainment_t {
-     uint32_t baseAddress;} DataContainment;
- 
+typedef struct DataContainment_t 
+{
+     uint32_t baseAddress;
+} DataContainment;
+
+ 
 // Calculates the resulting address of the given base address stored in the struct and an offset
 uint32_t getResultingAddress(uint32_t offset, const DataContainment & data_containmet);
- 
+
+ 
 // Define exception for out of range memory accesses
-struct MemoryOutOfRangeError:public std::exception {    const char *what() const noexcept {
+struct MemoryOutOfRangeError:public std::exception {
+    const char *what() const noexcept {
         return "MemoryOutOfRangeError Exception";
-    }};
-  
+    }
+};
+ 
+ 
 #endif // EMBEDDED_CONTROLLER_H
